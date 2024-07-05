f4f18b3e21f4c2ea61ee9e084edf744265cb8ae4
DEMkillstho <khalak3amak@gmail.com>
2024-07-05 11:56:30 +0300

    Addedd home in veiws

--- APP/TYF/views.py
+++ APP/TYF/views.py
@@ -4,3 +4,6 @@ from .models import ExampleModel
 def index(request):
     examples = ExampleModel.objects.all()
     return render(request, 'TestYourFluency/index.html', {'examples': examples})
+
+def home(request):
+    return
