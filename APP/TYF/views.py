4c65d1b71bf07152894d475335b0497a330a3231
DEMkillstho <khalak3amak@gmail.com>
2024-07-05 11:46:25 +0300

    init commit

---
+++ APP/TYF/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import ExampleModel
+
+def index(request):
+    examples = ExampleModel.objects.all()
+    return render(request, 'TestYourFluency/index.html', {'examples': examples})
