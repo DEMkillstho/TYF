2e886bfc39f625ac6927640b96a9a698af851c65
DEMkillstho <khalak3amak@gmail.com>
2024-07-09 10:57:58 +0300

    Delete APP directory

--- APP/TYF/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import ExampleModel
-
-def index(request):
-    examples = ExampleModel.objects.all()
-    return render(request, 'TestYourFluency/index.html', {'examples': examples})
-
-def home(request):
-    return
